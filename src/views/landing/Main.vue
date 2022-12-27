076e84b66ad45e7fc20941546fa0e43294d19f76
gurpreetkaur1998 <gurpreetkaur@codenomad.net>
2022-12-28 05:01:43 +0530

    changes

--- src/views/landing/Main.vue
+++ src/views/landing/Main.vue
@@ -2,124 +2,101 @@
   <div>
 
     <Header></Header>
-    <div class=" -mx-5 intro-y space landing_banner">
+    
+    <div class="-mx-5 intro-y space">
+      <TinySlider
+                :options="{
+                  mode: 'gallery',
+                  controls: true,
+                  speed: 500,
+                }"
+              >
+                <div class="px-2">
+                  <div class="image-fit rounded-md overflow-hidden" style="height: 500px;">
+                    <img
+                      alt="Midone Tailwind HTML Admin Template"
+                      :src="$f()[0].images[0]" 
+                    />
+                    <div class="slider-content">
+                      <h2 class="text-3xl text-left font-bold leading-none large-tittle space mt-3">EASY <br/> <span style="color:#f1ba22"> ACCURATE, SMART </span> <br/> AUTOMATED, WAREHOUSE</h2>
+                      <p class="text-lg my-3 space"><b> Automated vertical and horizontal warehouses: </b> <br/> the right step towards automating your warehouse!</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="px-2">
+                  <div class="image-fit rounded-md overflow-hidden" style="height: 500px;">
+                    <img
+                      alt="Midone Tailwind HTML Admin Template"
+                      :src="$f()[1].images[1]" 
+                    />
+                    <div class="slider-content">
+                      <h2 class="text-3xl text-left font-bold leading-none large-tittle space mt-3">EASY <br/> <span style="color:#f1ba22"> ACCURATE, SMART </span> <br/> AUTOMATED, WAREHOUSE</h2>
+                      <p class="text-lg my-3 space"><b> Automated vertical and horizontal warehouses: </b><br/> the right step towards automating your warehouse!</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="px-2">
+                  <div class="image-fit rounded-md overflow-hidden" style="height: 500px;">
+                    <img
+                      alt="Midone Tailwind HTML Admin Template"
+                      :src="$f()[2].images[2]" 
+                    />
+                    <div class="slider-content">
+                      <h2 class="text-3xl text-left font-bold leading-none large-tittle space mt-3">EASY <br/> <span style="color:#f1ba22"> ACCURATE, SMART </span> <br/> AUTOMATED, WAREHOUSE</h2>
+                      <p class="text-lg my-3 space"><b> Automated vertical and horizontal warehouses: </b><br/> the right step towards automating your warehouse!</p>
+                    </div>
+                  </div>
+                </div>
+      </TinySlider>
+    </div>                             
+    
+                                    
+                                
+    <!-- <div class=" -mx-5 intro-y space landing_banner">
       <h1 class="font-medium text-left space text-3xl mt-3">TAKE YOUR MATERIAL HANDLING</h1>
       <h2 class="text-3xl text-left font-bold leading-none large-tittle space mt-3">TO THE NEXT LEVEL</h2>
       <p class="text-lg mb-3 space">Transform Your Material Handling With The Most Advanced
         Automated Storage And Retrieval Systems From Modula.</p>
         <button class="btn btn-dark space">Explore Our Solutions</button>
+    </div> -->
+    <div class="my-5 md:mx-6 intro-y">
+      <div class="intro-y grid grid-cols-12 gap-6 mt-5">
+        <div class="col-span-6 md:col-span-6 space text-left">
+         <h1 class="font-medium text-left space text-3xl mx-auto mt-3">Benefits of Modula automated vertical warehouses:</h1>
+         <div class="saprater-left"></div>
+         <p class="text-lg mb-3">Save up to 90% of occupied floor space</p>
+         <p class="text-lg mb-3">Save time on picking operations</p>
+         <p class="text-lg mb-3">Use software integrated with corporate information systems (ERP)</p>
+         <p class="text-lg mb-3">Maintain constant control over goods movements, picking and refilling </p>
+         <p class="text-lg mb-3">Draw up an inventory with just one click without impeding operations</p>
+         <p class="text-lg mb-3">Increase picking accuracy, with a lower error rate</p>
+        </div>
+        <div class="col-span-6 md:col-span-6 text-center">
+          <img src="https://www.modula.eu/wp-content/uploads/2022/06/CCC_0059-1200x800-1-768x512-c-center.webp" class="p-5"/>
+        </div>
+      </div>
+      
     </div>
     <div class="my-5 md:mx-6 intro-y">
-      <h2 class="text-3xl text-center font-bold leading-none large-tittle space mt-3">WELCOME TO MIST</h2>
-      <div class="saprater"></div>
       <div class="intro-y grid grid-cols-12 gap-6 mt-5">
-        <div class="col-span-12 md:col-span-12 text-center">
-          <p class="text-lg mb-3 space">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quisquam,
-            culpa, soluta hic <br /> aperiam porro ipsum nisi optio necessitatibus commodi dolorum ente voluptatem
-            officiis similique maiores!</p>
+      
+        <div class="col-span-6 md:col-span-6 text-center">
+          <img src="https://www.modula.eu/wp-content/uploads/2022/06/magazzini_module_f-800x534-1-768x513-c-center.webp" class="p-5"/>
+        </div>
+        <div class="col-span-6 md:col-span-6 space text-left">
+         <h1 class="font-medium text-left space text-3xl mx-auto mt-3">Modula vertical and horizontal warehouses offer:</h1>
+         <div class="saprater-left"></div>
+         <p class="text-lg mb-3">Ad hoc solutions for all types of warehouse</p>
+         <p class="text-lg mb-3">Rapid, safe implementation into your warehouse workflow</p>
+         <p class="text-lg mb-3">The option to increase your number of warehouses</p>
+         <p class="text-lg mb-3">Staff preparation for hardware and software training</p>
+         <p class="text-lg mb-3">Outstanding pre- and after-sales assistance service</p>
+         <p class="text-lg mb-3">Two decades of experience in integration between Modula WMS and ERP</p>
+         <p class="text-lg mb-3">Quick and easy integration with other technologies</p>
+         <p class="text-lg mb-3">Technical and commercial presence across Italy and internationally</p>
         </div>
       </div>
-      <div class="grid grid-cols-12 gap-6 mt-5">
-        <!-- BEGIN: FAQ Menu -->
-        <a href="" class="intro-y col-span-12 lg:col-span-6 box ">
-          <img src="../../assets/images/business.jpg" class="w-full h-full rounded">
-        </a>
-        <div class="intro-y col-span-12 lg:col-span-6 box py-10 ">
-          <div class="font-medium text-center space w-3/4 text-3xl mx-auto mt-3">TELL YOUR DREAMS<br />
-            WE MAKE IT REAL</div>
-          <div class="saprater"></div>
-          <div class="text-slate-500 mt-2 space w-3/4 text-left mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus
-            malesuada facilisis. Nunc consectetur odio sed dolor tincidunt porttitor. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis. Nunc
-            consectetur odio sed dolor tincidunt porttitor consectetur adipiscing elit. Vestibulum nec odio ipsum.
-            Suspendisse cursus malesuada facilisis. porttitor consecteturadipiscing elit. Vestibulum nec odio ipsum.
-            Suspendisse cursus malesuada facilisis. Nunc consectetur odio sed dolor tincidunt porttitor consectetur
-            adipiscing elit.
-          </div>
-          <div class= " space text-slate-500 mt-2 w-3/4 text-left mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus
-            malesuada facilisis. Nunc consectetur odio sed dolor tincidunt porttitor. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit.
-          </div>
-
-        </div>
-
-        <!-- END: FAQ Menu -->
-
-      </div>
-      <div class="grid grid-cols-12 gap-6 mt-5">
-        <div class="col-span-12 lg:col-span-4 md:col-span-4 box py-4">
-          <div class="flex justify-center">
-            <RefreshCcwIcon class="block w-6 h-6 text-primary " />
-            <div class="font-medium space text-center text-base ml-3">Updates</div>
-          </div>
-
-          <div class="text-slate-500 space mt-2 w-3/4 text-center mx-auto">
-            Lorem ipsum dolor sit amet elit. consectetur Vest
-          </div>
-        </div>
-        <div class="col-span-12 lg:col-span-4 md:col-span-4 box py-4">
-          <div class="flex justify-center">
-            <SettingsIcon class="block w-6 h-6 text-primary" />
-            <div class="font-medium text-center text-base ml-3 space">Flexible</div>
-          </div>
-          <div class="text-slate-500 mt-2 space w-3/4 text-center mx-auto">
-            Lorem ipsum dolor sit amet elit. consectetur Vest
-          </div>
-        </div>
-        <div class="col-span-12 lg:col-span-4 md:col-span-4 box py-4">
-          <div class="flex justify-center">
-            <Edit2Icon class="block w-6 h-6 text-primary" />
-            <div class="font-medium text-center space text-base ml-2">Creative</div>
-          </div>
-          <div class="text-slate-500 mt-2 w-3/4 space text-center mx-auto">
-            Lorem ipsum dolor sit amet elit. consectetur Vest
-          </div>
-        </div>
-
-      </div>
-    </div>
-    <div class="py-5 md:mx-6 intro-y">
-
-      <div class="intro-y grid grid-cols-12 gap-6 mt-5">
-        <div class="col-span-12 md:col-span-6 lg:col-span-6 cursor-pointer zoom-in box p-5 justify-center">
-          <h2 class="text-3xl font-medium leading-none space mt-3">OUR PRODUCTS AND <br /> SERVICES</h2>
-
-          <br />
-          <div class="saprater-left"></div>
-          <p class="space">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus
-            malesuada facilisis. Nunc consectetur odio sed dolor tincidunt porttitor. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis. Nunc
-            consectetur odio sed dolor tincidunt porttitor
-            consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis.
-          </p>
-        </div>
-        <div class="col-span-12 md:col-span-6 lg:col-span-6 cursor-pointer zoom-in box p-5 justify-center">
-
-          <h2 class="space">DEVELOPMENT</h2>
-          <div class="progress my-3">
-            <div class="progress-bar w-1/2" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            </div>
-          </div>
-          <h2 class="space">DESIGNING</h2>
-          <div class="progress my-3">
-            <div class="progress-bar w-2/3 bg-success" role="progressbar" aria-valuenow="0" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-          <h2 class="space">MARKETING</h2>
-          <div class="progress my-3">
-            <div class="progress-bar w-3/4 bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-          <h2 class="space">CREATIVITY</h2>
-          <div class="progress my-3">
-            <div class="progress-bar w-3/4 bg-danger" role="progressbar" aria-valuenow="0" aria-valuemin="0"
-              aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
+     
     </div>
     <div class="py-5 md:mx-6 intro-y">
       <h2 class="text-3xl font-medium leading-none mt-3 space">AWESOME FEATURES</h2>
